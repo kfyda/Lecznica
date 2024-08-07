9cf0f39479e199411dd6c1813ab53f25961c98af
Kamil Fyda <kf8957@student.pwsz-ns.edu.pl>
2024-08-07 13:55:58 +0200

    Stopka

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,7 +8,8 @@
         <!-- Centralne informacje -->
         <div class="text-center mb-4 w-full md:w-1/3">
             <h3 class="text-2xl font-bold mb-2">Lecznica Weterynaryjna "SOCZEK"</h3>
-            <p class="text-sm">Zapewniamy najlepszą opiekę dla Twoich zwierząt</p>
+{{--            <p class="text-sm">Zapewniamy najlepszą opiekę dla Twoich zwierząt</p>--}}
+            <p class="text-sm">Kotek, piesek, koń czy krowa się przy Soczku dobrze chowa...</p>
         </div>
 
         <!-- Sekcja z danymi kontaktowymi -->
@@ -16,26 +17,26 @@
             <div class="mb-6 md:mb-0">
                 <h4 class="text-xl font-semibold mb-2">Kontakt</h4>
                 <div class="flex items-center mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 mr-2" viewBox="0 0 24 24">
-                        <path d="M12 2C8.69 2 6 4.69 6 8c0 3.87 3.13 7 7 7s7-3.13 7-7c0-3.31-2.69-6-6-6zm0 12c-2.76 0-5-2.24-5-5 0-2.76 2.24-5 5-5 2.76 0 5 2.24 5 5 0 2.76-2.24 5-5 5zM2 20v-1c0-2.21 1.79-4 4-4h12c2.21 0 4 1.79 4 4v1H2z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="1 0 24 24" fill="currentColor" class="w-6 h-6 mr-2">
+                        <path d="M12 2C8.14 2 5 5.14 5 9c0 4.33 4.22 8.94 6.38 10.88.37.33.9.33 1.28 0C14.78 17.94 19 13.33 19 9c0-3.86-3.14-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                     </svg>
                     <p class="text-sm">Biała Niżna 345, Grybów, Polska</p>
                 </div>
                 <div class="flex items-center mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 mr-2" viewBox="0 0 24 24">
-                        <path d="M20 15.5c-1.25 0-2.45-.2-3.57-.56-.35-.11-.76-.03-1.04.21L13 17c-2.72-1.36-4.91-3.54-6.27-6.27l1.84-2.39c.24-.31.32-.72.21-1.09-.36-1.12-.56-2.32-.56-3.57C8.21 3.01 8.2 3 8.2 3H4.5C3.67 3 3 3.67 3 4.5 3 15.29 8.71 21 19.5 21c.83 0 1.5-.67 1.5-1.5v-3.7c0-.01-.01-.01-.01-.02zm-9.5 1c-.83 0-1.5.67-1.5 1.5S8.67 19.5 9.5 19.5 11 18.83 11 18 10.33 16.5 9.5 16.5z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-2">
+                        <path d="M6.62 10.79a15.3 15.3 0 006.59 6.59l2.2-2.2a1.01 1.01 0 011.11-.23c1.12.45 2.33.7 3.6.7.55 0 1 .45 1 1v3.5c0 .55-.45 1-1 1C10.24 22 2 13.76 2 3.5c0-.55.45-1 1-1H6.5c.55 0 1 .45 1 1 0 1.27.25 2.48.7 3.6.13.35.04.75-.24 1.03l-2.2 2.2z"/>
                     </svg>
                     <p class="text-sm">18 445 04 09</p>
                 </div>
                 <div class="flex items-center mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 mr-2" viewBox="0 0 24 24">
-                        <path d="M12 2C6.48 2 2 6.48 2 12c0 5.52 4.48 10 10 10s10-4.48 10-10c0-5.52-4.48-10-10-10zm1 17h-2v-2h2v2zm1.07-7.75L12 12v4h-2v-4l-1.07-1.07c-.39-.39-.59-.9-.59-1.41 0-1.1.9-2 2-2s2 .9 2 2c0 .51-.2 1.02-.59 1.41z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-2">
+                        <path d="M12 12.713L.015 3.427A2.988 2.988 0 0 1 3 2h18c1.19 0 2.229.667 2.984 1.427L12 12.713zM12 14.287l-2.063-1.592L2 20h20l-7.937-7.305L12 14.287zM0 4.697v14.605l7.938-7.937L0 4.697zm24 0l-7.938 7.938L24 19.302V4.697z"/>
                     </svg>
                     <p class="text-sm">soczek@post.pl</p>
                 </div>
                 <!-- Sekcja z logo Facebooka -->
-                <div class="flex items-center mb-6 md:mb-0 w-full md:w-1/3 md:justify-start">
-                    <a href="https://www.facebook.com/Lecznica.Soczek/?locale=pl_PL" class="text-gray-400 hover:text-white">
+                <div class="flex items-center mb-6 md:mb-0 w-full md:justify-end pr-4 md:pr-20 mt-12">
+                    <a href="https://www.facebook.com/Lecznica.Soczek/?locale=pl_PL" class="text-blue-600 hover:text-blue-400 transition-colors">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-16 h-16" viewBox="0 0 24 24">
                             <path d="M22.675 0H1.325C.594 0 0 .593 0 1.326v21.348C0 23.406.594 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.464.099 2.797.143v3.243l-1.918.001c-1.504 0-1.794.714-1.794 1.76v2.308h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.325-.594 1.325-1.326V1.326C24 .593 23.406 0 22.675 0z"/>
                         </svg>
@@ -47,6 +48,6 @@
 
     <!-- Prawa autorskie na dole -->
     <div class="text-center">
-        <p class="text-sm text-gray-500">© 2024 Lecznica Weterynaryjna. Wszelkie prawa zastrzeżone.</p>
+        <p class="text-sm text-gray-500">© 2024 Lecznica Weterynaryjna "SOCZEK". Wszelkie prawa zastrzeżone.</p>
     </div>
 </footer>
