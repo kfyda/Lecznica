6d0f0d2fe9cd74c0467a90d5351e8650edbcc913
Rafal <rbanka221@gmail.com>
2024-08-08 14:05:05 +0200

    modyfikacja widoku ogloszen

--- resources/views/components/news.blade.php
+++ resources/views/components/news.blade.php
@@ -1,11 +1,9 @@
-
-    <div class="flex flex-col justify-between gap-x-5 md:flex-row bg-white p-4 rounded-md">
-            {{--Zdjęcie--}}
-            <img alt="{{ $news->slug }}" src="{{ $news->getURLImage() }}" class="w-full md:w-1/3 rounded-md">
-            {{--Ogłoszenie 1--}}
-            <article class="w-full">
-                <h3 class="text-2xl mt-6 md:mt-0 font-semibold mb-3">{{ $news->title }}</h3>
-                <p class="text-justify text-lg">{!! $news->getShortDescription(64) !!}</p>
-            </article>
-    </div>
-
+<div class="w-full bg-white p-3">
+        {{--Zdjęcie--}}
+        <img alt="{{ $news->slug }}" src="{{ $news->getURLImage() }}" class="mx-auto rounded-md">
+        {{--Ogłoszenie 1--}}
+        <article class="w-full h-72 mt-4 p-2">
+            <h4 class="text-2xl font-bold text-nowrap text-ellipsis overflow-hidden">{{ $news->title }}</h4>
+            <p class="mt-2 text-justify">{!! $news->getShortDescription(64) !!}</p>
+        </article>
+</div>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,13 +2,34 @@
     <div class="p-6">
         <h1 class="text-[64px] text-green-500 text-center font-semibold underline underline-offset-[16px]">Ogłoszenia</h1>
 
-        <section class="flex flex-col w-full justify-center mt-6 p-6 space-y-12">
+        <section class="flex flex-col justify-center mt-6 p-6 space-y-12">
             {{--Ogłoszenia--}}
-            @foreach($newsCollection as $news)
-                <a href="{{route('news.show', $news)}}">
-                    <x-news :news="$news" wire:key="{{ $news->id }}" />
-                </a>
-            @endforeach
+
+            {{--Najnowsze ogłoszenie--}}
+            <div>
+                <h2 class="text-4xl font-semibold mb-4">Najnowsze ogłoszenie</h2>
+                <div class="flex flex-col justify-center lg:justify-between gap-x-5 lg:flex-row bg-white p-4 rounded-md">
+                    {{--Zdjęcie--}}
+                    <img alt="{{ $recentNews->slug }}" src="{{ $recentNews->getURLImage() }}" class="mx-auto rounded-md w-full md:w-2/3 lg:w-1/3">
+                    {{--Ogłoszenie 1--}}
+                    <article class="mt-4 lg:mt-0">
+                        <h3 class="text-2xl text-center mt-6 md:mt-0 font-semibold mb-3">{{ $recentNews->title }}</h3>
+                        <p class="text-justify text-lg">{!! $recentNews->getShortDescription(80) !!}</p>
+                    </article>
+                </div>
+            </div>
+
+            {{--Pozostałe ogłoszenia--}}
+            <div>
+                <h2 class="text-3xl font-semibold mb-4">Zobacz również pozostałe ogłoszenia</h2>
+                <div class="w-full flex flex-wrap">
+                    @foreach($newsCollection as $news)
+                        <a class="md:w-1/2 lg:w-1/3" href="{{route('news.show', $news)}}">
+                            <x-news :news="$news" wire:key="{{ $news->id }}" />
+                        </a>
+                    @endforeach
+                </div>
+            </div>
         </section>
     </div>
 </x-app-layout>
