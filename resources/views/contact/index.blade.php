019da2985d7dfed1471a0f92e84c7d45b283177b
Kamil Fyda <kf8957@student.pwsz-ns.edu.pl>
2024-08-10 12:59:21 +0200

    PPies na czołówkę i kontakt gotowy

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,8 +6,67 @@
         </h1>
     </div>
 
-    <!-- Mapa na pełną stronę -->
-    <div id="map" class="w-full h-screen"></div>
+    <!-- Kontener na godziny pracy i mapę -->
+    <div class="flex flex-col md:flex-row">
+        <!-- Godziny pracy -->
+        <div class="w-full md:w-1/2 p-6 bg-gray-100 border-r border-gray-300">
+            <h2 class="text-2xl font-semibold mb-4 text-center">Godziny pracy</h2>
+            <div class="space-y-2">
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>poniedziałek:</strong></span>
+                    <span>08:00 - 17:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>wtorek:</strong></span>
+                    <span>08:00 - 17:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>środa:</strong></span>
+                    <span>08:00 - 17:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>czwartek:</strong></span>
+                    <span>08:00 - 17:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>piątek:</strong></span>
+                    <span>08:00 - 17:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>sobota:</strong></span>
+                    <span>08:00 - 13:00</span>
+                </div>
+                <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                    <span><strong>niedziela:</strong></span>
+                    <span class="text-red-500 font-bold">ZAMKNIĘTE</span>
+                </div>
+            </div>
+
+            <!-- Dane do kontaktu -->
+            <div class="mt-6">
+                <h2 class="text-2xl font-semibold mb-4 text-center">Dane do kontaktu</h2>
+                <div class="space-y-2">
+                    <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md">
+                        <span><strong>Adres lecznicy:</strong></span>
+                        <a href="https://www.google.com/maps/place/Lecznica+Weterynaryjna+%22Soczek%22/@49.6280425,20.9572105,134m/data=!3m1!1e3!4m6!3m5!1s0x473dc2822f10277f:0xc76b84267a0a0310!8m2!3d49.6281411!4d20.9574381!16s%2Fg%2F1tg_kdq4?entry=ttu" class="text-black-500 hover:underline">Biała Niżna 345, Grybów, Polska</a>
+                    </div>
+                    <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md mt-2">
+                        <span><strong>Telefon:</strong></span>
+                        <a href="tel:184450409" class="text-black-500 hover:underline">18 445 04 09</a>
+                    </div>
+                    <div class="flex justify-between px-4 py-2 bg-white shadow-sm rounded-md mt-2">
+                        <span><strong>Adres e-mail:</strong></span>
+                        <a href="mailto:soczek@post.pl" class="text-black-500 hover:underline">soczek@post.pl</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mapa -->
+        <div class="w-full md:w-1/2 p-8 flex justify-center md:justify-end">
+            <div id="map" class="w-full md:w-[700px] h-[600px] bg-gray-200"></div>
+        </div>
+    </div>
 
     <!-- Skrypt Google Maps API -->
     <script
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
-<main class="flex justify-center items-center min-h-screen bg-cover bg-center text-[#1e212b]" style="background-image: url({{ asset('Images/kot2.jpeg') }})">
-    <div class="bg-[#00000080] rounded-lg p-8 md:p-12 max-w-4xl text-center space-y-8 select-none">
+<main class="flex justify-center items-center min-h-screen bg-cover bg-center text-[#1e212b]" style="background-image: url({{ asset('Images/pies6.jpg') }})">
+    <div class="bg-[#00000080] rounded-lg p-6 md:p-9 max-w-3xl text-center space-y-6 select-none ml-auto mr-2 transform scale-75">
         <h1 class="text-6xl text-white font-bold">Przychodnia weterynaryjna "Soczek"</h1>
-        <h2 class="text-3xl text-green-400">Lekarz Weterynarii Janusz Soczek</h2>
+        <h2 class="text-3xl text-green-400">Lekarz Weterynarii - Janusz Soczek</h2>
         <div class="flex flex-wrap justify-center gap-4">
         </div>
     </div>
