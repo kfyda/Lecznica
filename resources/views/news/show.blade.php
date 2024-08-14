5fb7aff4ecd5cee5a86dbd0c3b9f4e78a2586b97
Rafal <rbanka221@gmail.com>
2024-08-14 16:27:58 +0200

    layout shop - w trakcie

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -3,14 +3,20 @@
         {{ 'Ogłoszenie ' . $news->title }}
     @endsection
 
-    <div class="flex justify-center bg-gray-700 mt-20">
-        <img alt="{{ $news->slug }}" src="{{ $news->getURLImage() }}" class="h-[70vh]" />
+    <div class="flex justify-center bg-green-500 mt-20">
+        @if($news->image_path)
+            <img alt="{{ $news->slug }}" src="{{ $news->getURLImage() }}" class="h-[70vh]" />
+        @else
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-[70vh] size-64">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+            </svg>
+        @endif
     </div>
     <div class="flex flex-col md:flex-row p-6 gap-x-4">
         <article class="md:w-3/4 p-4 md:border-r-2 md:border-gray-300/50 shadow-inner">
             <div class="">
                 <p class="lg:text-right">Dodano: &nbsp;<time>{{ $news->formatedDate() }} <br> O godzinie: {{ $news->formatedTime() }}</time></p>
-                <h2 class="text-4xl font-semibold text-green-600 mt-8 lg:mt-0">{{ $news->title }}</h2>
+                <h2 class="text-4xl font-semibold text-center text-green-600 mt-8 lg:mt-0">{{ $news->title }}</h2>
             </div>
             <p class="mt-12">{!! $news->description !!}</p>
         </article>
