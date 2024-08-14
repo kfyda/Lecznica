bbcc1dcf89f598ec1c02ba33044b16992f714cbf
Kamil Fyda <kf8957@student.pwsz-ns.edu.pl>
2024-08-14 10:44:48 +0200

    Zmiana mapy w kontakcie

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -63,28 +63,55 @@
 
         <!-- Mapa -->
         <div class="w-full md:w-1/2 p-6 flex justify-center md:justify-end">
-            <div id="map" class="w-full h-[400px] md:w-[800px] md:h-[600px] bg-gray-200"></div>
+            <div id="map" class="w-full h-[400px] md:w-[800px] md:h-[600px] bg-gray-200 z-0"></div>
         </div>
+
+        <!-- Skrypty Leaflet.js i CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+        <script>
+            function initMap() {
+                const location = [49.6281411, 20.9574381]; // Współrzędne lokalizacji
+                const map = L.map('map').setView(location, 15);
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '© OpenStreetMap contributors'
+                }).addTo(map);
+
+                // Niestandardowa czerwona ikona pinezki
+                const redIcon = L.icon({
+                    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-red.png',
+                    iconSize: [25, 41], // rozmiar ikony
+                    iconAnchor: [12, 41], // punkt kotwicy (dla wyrównania)
+                    popupAnchor: [1, -34], // gdzie pojawia się dymek
+                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+                    shadowSize: [41, 41] // rozmiar cienia
+                });
+
+                const marker = L.marker(location).addTo(map)
+                    .bindPopup("Lecznica Weterynaryjna 'Soczek'");
+
+                // Wyświetlanie dymka przy najechaniu na pinezkę
+                marker.on('mouseover', function() {
+                    marker.openPopup();
+                });
+
+                marker.on('mouseout', function() {
+                    marker.closePopup();
+                });
+
+                // Przekierowanie na Google Maps po kliknięciu na pinezkę
+                marker.on('click', function() {
+                    window.location.href = "https://www.google.com/maps/place/Lecznica+Weterynaryjna+%22Soczek%22/@49.6280425,20.9572105,134m/data=!3m1!1e3!4m6!3m5!1s0x473dc2822f10277f:0xc76b84267a0a0310!8m2!3d49.6281411!4d20.9574381!16s%2Fg%2F1tg_kdq4?entry=ttu";
+                });
+            }
+
+            // Inicjalizacja mapy po załadowaniu strony
+            document.addEventListener('DOMContentLoaded', function() {
+                initMap();
+            });
+        </script>
     </div>
-
-    <!-- Skrypt Google Maps API -->
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChoSm7vMADJiut434Gld8tRP4TjD8Al9U&callback=initMap&v=weekly"
-        defer
-    ></script>
-
-    <script>
-        function initMap() {
-            const location = { lat: 49.6281411, lng: 20.9574381 }; // Współrzędne lokalizacji
-            const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: location,
-            });
-            new google.maps.Marker({
-                position: location,
-                map: map,
-                title: "Lecznica Weterynaryjna 'Soczek'",
-            });
-        }
-    </script>
 </x-app-layout>
