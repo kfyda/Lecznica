4c6f1def9622aad2454e9db0795f1bb3b0941a37
Kamil Fyda <kf8957@student.pwsz-ns.edu.pl>
2024-08-14 15:55:56 +0200

    Wyłączenie trybu ciemnego

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    // darkMode: 'class',
+     darkMode: 'false',
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
